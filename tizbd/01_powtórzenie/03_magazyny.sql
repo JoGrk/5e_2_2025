39a82c41b890114adfa037b4f429c541f98092e4
Jolanta Gryko <jgryko@gmail.com>
2025-09-19 10:55:44 +0200

    19.09

---
+++ tizbd/01_powtórzenie/03_magazyny.sql
@@ -0,0 +1,162 @@
+-- Utwórz tabele:
+
+-- Magazyny
+-- Kod  całkowity, klucz podstawowy
+-- Lokalizacja tekst, nie puste
+-- Pojemność całkowita, nie puste
+
+CREATE TABLE Magazyny (
+    kod int PRIMARY KEY,
+    Lokalizacja varchar(255) NOT NULL,
+    pojemnosc int NOT NULL
+);
+
+-- Kontenery
+-- Kod tekst o stałej długości 4 znaków, klucz podstawowy,
+-- Zawartosc tekst
+-- Wartosc rzeczywista, dwa miejsca po przecinku, cztery przed
+-- Magazyn całkowita, nie pusta, pole klucza obcego (odwołuje się do pola kod w tabeli magazyny)
+CREATE TABLE Kontenery (
+    kod char(4) primary key,
+    zawartosc varchar(255),
+    wartosc dec(6,2),
+    magazyn int not null,
+    FOREIGN KEY(magazyn) REFERENCES magazyny(kod) 
+);
+
+ 
+
+-- Dodaj dane
+
+  INSERT INTO Magazyny(Kod,Lokalizacja,Pojemnosc) VALUES(1,'Chicago',3);
+ INSERT INTO Magazyny(Kod,Lokalizacja,Pojemnosc) VALUES(2,'Chicago',4);
+ INSERT INTO Magazyny(Kod,Lokalizacja,Pojemnosc) VALUES(3,'New York',7);
+ INSERT INTO Magazyny(Kod,Lokalizacja,Pojemnosc) VALUES(4,'Los Angeles',2);
+ INSERT INTO Magazyny(Kod,Lokalizacja,Pojemnosc) VALUES(5,'San Francisco',8);
+ INSERT INTO Magazyny(Kod,Lokalizacja,Pojemnosc) VALUES(6,'Chicago',3);
+ 
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('0MN7','Rocks',180,3);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn)  VALUES('4H8P','Rocks',250,1);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('4RT3','Scissors',190,4);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('7G3H','Rocks',200,1);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('8JN6','Papers',175,1);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('8Y6U','Papers',50,3);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('9J6F','Papers',175,2);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('LL08','Rocks',140,4);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('P0H6','Scissors',155,1);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('P2T6','Scissors',150,2);
+ INSERT INTO Kontenery(Kod,Zawartosc,Wartosc,Magazyn) VALUES('TU55','Papers',90,5);
+ 
+
+
+
+
+ 
+
+ 
+
+-- 1. Wyświetl wszystkie dane o wszystkich magazynach (zrzut)
+
+SELECT *
+FROM magazyny;
+-- 2. Wyświetl wszystkie kontenery o wartości większej niż $150.  (zrzut)
+SELECT *
+FROM kontenery
+WHERE wartosc > 150;
+ 
+-- 3. Co jest przechowywane w kontenerach? Wyświetl zawartość tak, aby dane się nie powtarzały.   (zrzut)
+ SELECT DISTINCT zawartosc
+ FROM kontenery;
+ 
+
+-- 4. Wyświetl średnią wartość wszystkich kontenerów.   (zrzut)
+SELECT AVG(wartosc)
+FROM kontenery;
+
+-- 5. Wyświetl kody magazynów wraz ze średnią wartością zawartych w nich kontenerów.  (zrzut)
+SELECT magazyn, AVG(wartosc)
+FROM kontenery
+GROUP BY magazyn;
+
+-- 6. Wyświetl kody magazynów wraz ze średnią wartością zawartych w nich kontenerów, ale ogranicz się do tych, w których średnia wartość kontenerów jest większa niż 150.  (zrzut)
+SELECT magazyn, AVG(wartosc)
+FROM kontenery
+GROUP BY Magazyn
+HAVING AVG(wartosc) > 150;
+
+
+-- 7. Wyświetl kod każdego kontenera, wraz z miastem, gdzie jest zlokalizowany (czyli lokalizacją magazynu)  (zrzut)
+select kontenery.kod, Lokalizacja
+from magazyny
+    join kontenery on magazyny.kod=kontenery.magazyn;
+-- 8. Wyświetl  kody magazynów wraz z liczbą kontenerów w każdym z tych magazynów.  (zrzut)
+SELECT magazyn, COUNT(wartosc) AS kontenery
+FROM kontenery
+GROUP BY magazyn;
+-- 9. Rozwiń poprzednie zapytanie tak, aby w zestawieniu były wymienione również magazyny, w których nie ma kontenerów (powinna być wyświetlona liczba zero, a nie magazyn pominięty w tym zestawieniu)  (zrzut)
+
+        SELECT magazyny.kod, COUNT(Kontenery.kod)
+        FROM kontenery RIGHT JOIN magazyny ON Kontenery.Magazyn = Magazyny.kod
+        GROUP BY magazyn;
+
+
+
+-- Wskazówka: wybierz kod magazynu z tabeli magazyny oraz policz kod z tabeli Kontenery; dane wybieraj z dwóch tabel połączonych połączeniem rozszerzającym (LEFT JOIN lub RIGHT JOIN), z rozszerzanej strony powinna być tabela Magazyny (wybieramy wszystkie magazyny, także te, w których nie ma kontenerów); grupowanie według kodu magazynu
+
+ 
+
+ 
+
+-- 10.  Wyświetl kody wszystkich magazynów, które są przeładowane (magazyn jest przeładowany, jeśli liczba zawartych w nim kontenerów jest większa niż jego pojemność)  (zrzut)
+SELECT kod
+FROM magazyny
+WHERE pojemnosc <(SELECT COUNT(*)
+                    FROM kontenery
+                    WHERE magazyn = magazyny.kod);
+ 
+
+-- np. wyświetl kody tych magazynów (z tabeli Magazyny), których pojemność jest mniejsza niż
+
+-- (podzapytanie) ilość kontenerów (z tabeli kontenery), których kod magazynu (pole magazyn) jest taki jak kod magazynu z tabeli głównego zapytania (czyli Magazyny)
+
+
+-- 11. Wyświetl kody wszystkich kontenerów zlokalizowanych w Chicago wraz z lokalizacją i pojemnością magazynów . Uwzględnij w zestawieniu także te magazyny z Chicago, w których nie ma aktualnie kontenerów  (zrzut)
+
+SELECT kontenery.kod,Lokalizacja,pojemnosc
+FROM kontenery
+RIGHT JOIN magazyny ON Kontenery.Magazyn = Magazyny.kod
+WHERE Lokalizacja = 'Chicago';
+
+   
+
+-- lub zastosuj podzapytanie (wyświetl kody kontenerów, dla których kod magazynu (magazyn) jest na liście (IN) kodów magazynów zlokalizowanych w Chicago. 
+
+-- 12. Utwórz nowy magazyn w Nowym Yorku z pojemnością 3 kontenerów.   (zrzut)
+
+INSERT INTO magazyny(Lokalizacja, pojemnosc)
+VALUES
+('Nowy York', 3);
+
+
+-- 13. Utwórz nowy kontener, z kodem "H5RT",  zawierającym "Papers" z wartością $200 i zlokalizowany w magazynie o kodzie 2.  (zrzut)
+INSERT INTO kontenery
+VALUES
+('H7RT', 'Papers', 200, 2);
+-- 14. Zmniejsz wartość wszystkich kontenerów o 15%.  (zrzut)
+update kontenery
+set wartosc=wartosc*0.85;
+-- 15 Usuń wszystkie kontenery o wartości mniejszej niż $100.  (zrzut)
+DELETE 
+FROM Kontenery
+WHERE wartosc < 100;
+-- 16. Usuń wszystkie kontenery z przeładowanych magazynów.   (zrzut)
+
+        DELETE 
+        FROM Kontenery
+        WHERE magazyn IN (SELECT kod
+                        FROM magazyny
+                        WHERE pojemnosc <(SELECT COUNT(*)
+                                FROM kontenery
+                                WHERE magazyn = magazyny.kod));
+
+-- usuń wszystkie kontenery, których kod jest na liście (IN) kodów przeładowanych magazynów (tę wartość zwraca zapytanie nr. 10)
---
+++ tizbd/01_powtórzenie/04_kina.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE 5e_2_kina;
+USE 5e_2_kina;
+-- 1. Przygotuj tabele:
+
+-- Filmy
+-- Kod liczba całkowita, autoinkrementacja, klucz podstawowy
+-- Tytul  tekst
+-- Rating tekst
+CREATE TABLE Filmy (
+    kod INT PRIMARY KEY AUTO_INCREMENT,
+    tytul varchar(255),
+    rating varchar(255)
+);
+
+
+
+-- Kina
+-- Kod  liczba całkowita, autonumerowanie, klucz podstawowy
+-- Nazwa tekst nie puste
+-- Film liczba całkowita, klucz obcy odwołujący się do pola kod w tabeli Filmy
+
+CREATE TABLE kina(
+    kod int AUTO_INCREMENT PRIMARY KEY,
+    nazwa varchar(255) NOT NULL,
+    film int,
+    FOREIGN KEY (film) REFERENCES filmy(kod)
+);
+ 
+
+  
+
+ INSERT INTO Filmy(Kod,Tytul,Rating) VALUES(9,'Citizen King','G');
+ INSERT INTO Filmy(Kod,Tytul,Rating) VALUES(1,'Citizen Kane','PG');
+ INSERT INTO Filmy(Kod,Tytul,Rating) VALUES(2,'Singin'' in the Rain','G');
+ INSERT INTO Filmy(Kod,Tytul,Rating) VALUES(3,'The Wizard of Oz','G');
+ INSERT INTO Filmy(Kod,Tytul,Rating) VALUES(4,'The Quiet Man',NULL);
+ INSERT INTO Filmy(Kod,Tytul,Rating) VALUES(5,'North by Northwest',NULL);
+ INSERT INTO Filmy(Kod,Tytul,Rating) VALUES(6,'The Last Tango in Paris','NC-17');
+ INSERT INTO Filmy(Kod,Tytul,Rating) VALUES(7,'Some Like it Hot','PG-13');
+ INSERT INTO Filmy(Kod,Tytul,Rating)  VALUES(8,'A Night at the Opera',NULL);
+ 
+ INSERT INTO Kina(Kod,Nazwa,Film) VALUES(1,'Odeon',5);
+ INSERT INTO Kina(Kod,Nazwa,Film) VALUES(2,'Imperial',1);
+ INSERT INTO Kina(Kod,Nazwa,Film) VALUES(3,'Majestic',NULL);
+ INSERT INTO Kina(Kod,Nazwa,Film) VALUES(4,'Royale',6);
+ INSERT INTO Kina(Kod,Nazwa,Film) VALUES(5,'Paraiso',3);
+ INSERT INTO Kina(Kod,Nazwa,Film) VALUES(6,'Nickelodeon',NULL);
+ 
+
+ 
+
+ 
+
+ 
+
+-- 1. Wyświetl tytuły wszystkich filmów
+
+SELECT tytul FROM filmy;
+
+-- 2. Ile różnych poziomów ratingu jest zapisane w bazie? Wyświetl je (dane nie powinny się powtarzać). 
+SELECT DISTINCT rating 
+FROM filmy;
+
+-- 3. Wyświetl wszystkie filmy z nieustalonym ratingiem.
+SELECT tytul
+FROM filmy
+WHERE rating IS NULL;
+-- 4. Wyświetl wszystkie  kina, w których aktualnie nie są wyświetlane żadne filmy. 
+SELECT nazwa
+FROM kina
+WHERE film IS NULL;
+-- 5. Wyświetl wszystkie dane o kinach i filmach, uwzględnij tylko dane o filmach, które są wyświetlane w tych kinach i dane o kinach, w których są wyświetlane jakieś filmy. 
+select *
+from kina
+    join filmy on kina.film=filmy.kod;
+-- 6. Wyświetl wszystkie dane o kinach i filmach, uwzględnij w zestawieniu także te kina, w których aktualnie nie są wyświetlane żadne filmy.
+select *
+from kina  LEFT join filmy on kina.film=filmy.kod
+    
+-- 7. Wyświetl wszystkie dane o kinach i filmach, uwzględnij w zestawieniu także te filmy, które nie są aktualnie wyświetlane w żadnych kinach. 
+select *
+from kina
+    right join filmy on kina.film=filmy.kod;
+-- 8. Wyświetl wszystkie dane o kinach i filmach, uwzględnij w zestawieniu także te kina, w których aktualnie nie są wyświetlane żadne filmy oraz  te filmy, które nie są aktualnie wyświetlane w żadnych kinach. 
+select *
+from kina  LEFT join filmy on kina.film=filmy.kod
+UNION 
+select *
+from kina
+    right join filmy on kina.film=filmy.kod;
+-- 9. Dodaj film  "One, Two, Three" (bez ratingu)
+ 
+-- 10. Ustal rating wszystkich filmów bez ratingu na  "G".
+
+-- 11. usuń kina wyświetlające filmy z ratingiem "NC-17".

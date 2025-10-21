1ab56a6ccfce6885cafd11075dda0fff89cc06e4
Jolanta Gryko <jgryko@gmail.com>
2025-10-21 11:12:43 +0200

    21.10

---
+++ bazy/02_formularze/03_portal EE.09-05-22.01-SG/uzytkownicy.php
@@ -0,0 +1,56 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal społecznościowy</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+    <header>
+        <section class="banner-left">
+            <h2>Nasze osiedle</h2>
+        </section>
+
+        <section class="banner-right">
+            <!-- skrypt1 -->
+       
+
+
+        </section>
+    </header>
+
+
+    <main>
+        <section class="main-left">
+            <h3>Logowanie</h3>
+            <form action="" method="post">
+                <label for="login">login</label><br>
+                <input type="text" name="login" id="login">
+                <label for="password">password</label><br>
+                <input type="password" name="password" id="password">
+                <button>Zaloguj</button>
+            </form>
+        </section>
+        <section class="main-right">
+            <h3>Wizytówka</h3>
+            <section class="profile">
+                <!-- skrypt 2 -->
+               
+            </section>
+        </section>
+    </main>
+    
+    <footer>Stronę wykonał: 00000000000</footer>
+    
+</body>
+</html>
+
+<?php
+$link -> close();
+?>

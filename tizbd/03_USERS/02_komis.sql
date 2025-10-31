24ee4def3b9e860d63ae07ec4ec64069825d5a45
Jolanta Gryko <jgryko@gmail.com>
2025-10-31 11:50:51 +0100

    31.10

--- tizbd/03_USERS/02_komis.sql
+++ tizbd/03_USERS/02_komis.sql
@@ -30,14 +30,28 @@ GRANT ALL ON 5e_2_komis.Klienci TO 'uczendba_2';
 GRANT ALL ON 5e_2_komis.Handlowcy TO 'uczendba_2';
 
 -- B. zabroń mu usuwania rekordów z tabeli Klienci 
--- C. odbierz uprawnienia wykonywania zapytań usuwania rekordów i modyfikowania wartości w tabeli Handlowcy
+-- DENY DELETE ON 5e_2_komis.Klienci TO 'uczendba_2';
+REVOKE DELETE ON 5e_2_komis.Klienci FROM 'uczendba_2';
 
+-- C. odbierz uprawnienia wykonywania zapytań usuwania rekordów i modyfikowania wartości w tabeli Handlowcy
+REVOKE DELETE,UPDATE ON 5e_2_komis.Handlowcy FROM 'uczendba_2';
 -- 5. Utwórz użytkowników jeden i dwa (bez hasła).
+CREATE USER 'jeden_2';
+CREATE USER 'dwa_2';
 --  A. Nadaj uprawnienia wprowadzania, zmiany i usuwania danych w całej bazie komis dla użytkownika jeden
+GRANT INSERT,DELETE,UPDATE ON 5e_2_komis.* TO 'jeden_2';
 -- B. nadaj wszystkie uprawnienia do tabeli Auta użytkownikowi dwa
+GRANT ALL on 5e_2_komis.Auta to 'dwa_2';
 -- C. odbierz użytkownikowi jeden prawa usuwania danych 
+REVOKE DELETE ON 5e_2_komis.* FROM 'jeden_2';
 -- D. odbierz wszystkie uprawnienia użytkownikowi dwa do tabeli Auta
+REVOKE ALL ON 5e_2_komis.Auta FROM 'dwa_2';
 -- 6. 
 -- A. Zaloguj się jako użytkownik jeden i ustaw hasło 'zaq1@WSX'
+mysql -u jeden_2
+SET PASSWORD = password('zaq1@WSX');
+mysql -u jeden_2 -p
 -- B. Ustaw hasło dla użytkownika dwa na 'zaq1@WSX'
+SET password FOR  'dwa_2'= password('zaq1@WSX');
 -- C. z poziomu konta root zmień hasło dla użytkownika jeden na 'haslo'
+SET password FOR  'jeden_2'= password('haslo');
---
+++ tizbd/03_USERS/03_role.sql
@@ -0,0 +1,40 @@
+-- 1. Utwórz użytkownika u1  z hasłem abcd
+CREATE user 'u1' IDENTIFIED BY 'abcd';
+-- 2. Utwórz bazę danych b i wykonaj do niej plik komisF.sql(metodą przekierowania) 
+
+
+-- 3. Użytkownikowi u1 nadaj prawo do efektywnego wstawiania, usuwania i modyfikowania danych dla tabeli samochody.
+GRANT INSERT, DELETE, UPDATE, SELECT ON 5e_2_komis.Auta TO 'u1';
+
+-- 4. Użytkownikowi u1 odbierz prawo do usuwania danych w tabeli samochody
+REVOKE DELETE ON 5e_2_komis.Auta from 'u1';
+-- 5. Zablokuj koto u1
+alter user 'u1' account lock;
+-- 6. Odblokuj konto u1
+alter user 'u1' account unlock;
+-- 7.Ustaw hasło dla użytkownika u1 na 1234
+SET password FOR 'u1' = password('1234');
+-- 8, utwórz rolę r1 z prawami do wyświetlania danych z wszystkich tabel bazy b
+CREATE ROLE 'r1';
+GRANT SELECT ON 5e_2_komis.* TO 'r1'; 
+-- 9. Użytkownikowi u1 przypisz prawo do roli r1
+GRANT 'r1' TO 'u1';
+-- 10. zaloguj się jako u1 i sprawdź uprawniania (show grants)
+
+--        11. Sprawdź, czy możesz wyświetlić dane z tabeli zamowienia
+
+--        12. włącz (ustaw) rolę r1
+
+--        13. sprawdź, czy możesz wyświetlić dane z tabeli zamowienia
+
+--         14. sprawdź, czy możesz usunąć zamowienie o id 3
+
+-- 15. do roli r1 dodaj prawo do usuwania danych z tabeli zamowienia
+
+--          16. sprawdź uprawnienia
+
+--          17. usuń zamówienie o id 2
+
+-- 18. usuń rolę r1
+
+-- 19. usuń użytkownika u1

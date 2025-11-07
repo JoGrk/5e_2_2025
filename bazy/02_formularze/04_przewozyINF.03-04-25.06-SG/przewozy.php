07dd1c2d89abd60bb736c526875489ba43ccb5c1
Jolanta Gryko <jgryko@gmail.com>
2025-11-07 11:49:50 +0100

    7.11

---
+++ bazy/02_formularze/04_przewozyINF.03-04-25.06-SG/przewozy.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Firma Przewozowa</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Firma przewozowa Półdarmo</h1>
+    </header>
+
+    <nav>
+        <a href="kw1.png">kwerenda1</a>
+        <a href="kw2.png">kwerenda2</a>
+        <a href="kw3.png">kwerenda3</a>
+        <a href="kw4.png">kwerenda4</a>
+    </nav>
+
+    <main>
+        <section class="left">
+            <h2>Zadania do wykonania</h2>
+
+            <table>
+                <tr>
+                    <th>Zadanie do wykonania</th>
+                    <th>Data realizacji</th>
+                    <th>Akcja</th>
+                </tr>
+                <!-- s1 -->
+                <!-- s2 -->
+            </table>
+
+            <form action="" method="post">
+                <label for="f_task">Zadanie do wykonania:</label>
+                <input type="text" name="f_task" id="f_task"><br>
+                <label for="f_date">Data realizacji</label>
+                <input type="date" name="f_date" id="f_date">
+                <button type="submit">Dodaj</button>
+            </form>
+            
+        </section>
+
+        <section class="right">
+            <img src="auto1.jpg" alt="auto firmowe">
+            <h3>Nasza specjalnosc</h3>
+            <ul>
+                <li>Przeprowadzki:</li>
+                <li>Przewóz mebli</li>
+                <li>Przesyłki gabarytowe</li>
+                <li>Wynajem pojazdów</li>
+                <li>Zakupy towarów</li>
+            </ul>
+        </section>
+    </main>
+
+    <footer>
+        <p>Stronę wykonał:0002137</p>
+    </footer>
+</body>
+</html>
